0a235180e7d0c6dec55005c1ae2fdd42710e3b31
JerseysGet <dev.faisal2004@gmail.com>
2023-04-20 16:38:48 +0530

    switched to camelCase

--- src/script.js
+++ src/script.js
@@ -4,48 +4,48 @@ const countOccurences = (str, char) => {
   return str.split("").filter((c) => c === char).length;
 };
 
-const path_prefix =
+const pathPrefix =
   "./" + "../".repeat(countOccurences(window.location.pathname, "/") - 2);
-const get_path = (path) => {
-  return path_prefix + path;
+const getPath = (path) => {
+  return pathPrefix + path;
 };
 
-const get_nav = (selected) => {
+const getNav = (selected) => {
   const nav = document.createElement("nav");
 
-  const index_link_node = document.createElement("a");
-  index_link_node.href = get_path("index.html");
-  index_link_node.textContent = "HOME";
+  const indeLinkNode = document.createElement("a");
+  indeLinkNode.href = getPath("index.html");
+  indeLinkNode.textContent = "HOME";
 
-  const artists_link_node = document.createElement("a");
-  artists_link_node.href = get_path("artists.html");
-  artists_link_node.textContent = "ARTISTS";
+  const artistsLinkNode = document.createElement("a");
+  artistsLinkNode.href = getPath("artists.html");
+  artistsLinkNode.textContent = "ARTISTS";
 
-  const about_link_node = document.createElement("a");
-  about_link_node.href = get_path("about.html");
-  about_link_node.textContent = "ABOUT";
+  const aboutLinkNode = document.createElement("a");
+  aboutLinkNode.href = getPath("about.html");
+  aboutLinkNode.textContent = "ABOUT";
 
-  const search_link_node = document.createElement("a");
-  search_link_node.href = get_path("search.html");
-  search_link_node.textContent = "SEARCH";
+  const searchLinkNode = document.createElement("a");
+  searchLinkNode.href = getPath("search.html");
+  searchLinkNode.textContent = "SEARCH";
 
-  const dot_node = document.createElement("div");
-  dot_node.classList.add("dot");
+  const dotNode = document.createElement("div");
+  dotNode.classList.add("dot");
 
   if (selected === "index") {
-    index_link_node.classList.add("selected");
+    indeLinkNode.classList.add("selected");
   } else if (selected === "artists") {
-    artists_link_node.classList.add("selected");
+    artistsLinkNode.classList.add("selected");
   } else if (selected === "about") {
-    about_link_node.classList.add("selected");
+    aboutLinkNode.classList.add("selected");
   }
 
   nav.append(
-    index_link_node,
-    artists_link_node,
-    about_link_node,
-    search_link_node,
-    dot_node
+    indeLinkNode,
+    artistsLinkNode,
+    aboutLinkNode,
+    searchLinkNode,
+    dotNode
   );
 
   return nav;
@@ -56,16 +56,16 @@ const insertNavbar = () => {
   navbar.classList.add("navbar");
 
   const logo = document.createElement("img");
-  logo.src = get_path("assets/images/logo_big_light.png");
+  logo.src = getPath("assets/images/logo_big_light.png");
   logo.alt = "SoC";
   logo.classList.add("logo");
   navbar.appendChild(logo);
   const filename = window.location.pathname.split("/").pop().split(".")[0];
 
   if (["about", "artists", "index", "search"].includes(filename)) {
-    navbar.appendChild(get_nav(filename));
+    navbar.appendChild(getNav(filename));
   } else {
-    navbar.appendChild(get_nav());
+    navbar.appendChild(getNav());
   }
 
   body.insertBefore(navbar, body.firstChild);
@@ -75,7 +75,7 @@ const insertFooter = () => {
   const footer = document.createElement("footer");
 
   const aboutLink = document.createElement("a");
-  aboutLink.href = get_path("about.html");
+  aboutLink.href = getPath("about.html");
   aboutLink.textContent = "ABOUT";
 
   const footerText = document.createElement("div");
@@ -83,7 +83,7 @@ const insertFooter = () => {
   footerText.textContent = "Made by Praneeth & Faisal";
 
   const Logo = document.createElement("img");
-  Logo.src = get_path("assets/images/logo_small_light.png");
+  Logo.src = getPath("assets/images/logo_small_light.png");
   Logo.alt = "logo";
 
   footer.append(aboutLink, footerText, Logo);
@@ -150,12 +150,12 @@ const animateHeaders = () => {
         h.textContent = "";
         if (happening) return;
         happening = true;
-        const increment = (cur_pos) => {
-          if (cur_pos == final.length) {
+        const increment = (curPos) => {
+          if (curPos == final.length) {
             happening = false;
           } else {
-            h.textContent += final.charAt(cur_pos);
-            setTimeout(increment.bind(null, cur_pos + 1), 69);
+            h.textContent += final.charAt(curPos);
+            setTimeout(increment.bind(null, curPos + 1), 69);
           }
         };
         increment(0);
