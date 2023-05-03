4f92820191ac1e8496cce5b7bfb89eea44ad1fa4
PraneethJain <praneethjain005@gmail.com>
2023-05-03 12:04:23 +0530

    moved and added type annotations

--- src/song_db.py
+++ src/song_db.py
@@ -1,26 +1,25 @@
 import sqlite3
 
-primes = [p for p in range(2, 1000) if all(p % i != 0 for i in range(2, p))]
 
 class Database:
-    def __init__(self):
-        self.connection = sqlite3.connect('sonuscloud.db')
+    def __init__(self) -> None:
+        self.connection = sqlite3.connect("sonuscloud.db")
         self.cursor = self.connection.cursor()
 
-    def insert_song(self, song_id):
-        self.cursor.execute(f'INSERT INTO SONGS(SONG_ID) VALUES({song_id})')
+    def insert_song(self, song_id: int) -> None:
+        self.cursor.execute(f"INSERT INTO SONGS(SONG_ID) VALUES({song_id})")
         self.connection.commit()
 
-    def remove_song(self, song_id):
+    def remove_song(self, song_id: int) -> None:
         self.cursor.execute(f"SELECT * FROM SONGS WHERE SONG_ID = {song_id}")
         song = self.cursor.fetchall()
         if len(song) != 0:
             self.cursor.execute(f"DELETE FROM SONGS WHERE SONG_ID = {song_id}")
             self.connection.commit()
 
-    def get_songs(self):
+    def get_songs(self) -> list[int]:
         self.cursor.execute(f"SELECT * FROM SONGS")
         return [song[0] for song in self.cursor.fetchall()]
 
-    def close(self):
+    def close(self) -> None:
         self.connection.close()
