787b717b2246db98d2e7aa7f644b76888b8f807b
Jerseys <dev.faisal2004@gmail.com>
2023-05-03 14:37:23 +0530

    changed function parameters

--- src/song_db.py
+++ src/song_db.py
@@ -1,21 +1,41 @@
 import sqlite3
+import os
 
 
 class Database:
     def __init__(self) -> None:
         self.connection = sqlite3.connect("sonuscloud.db")
         self.cursor = self.connection.cursor()
+        if not self.__table_exists():
+            self.__create_table()
 
-    def insert_song(self, song_id: int) -> None:
-        self.cursor.execute(f"INSERT INTO SONGS(SONG_ID) VALUES({song_id})")
+    def __table_exists(self) -> bool:
+        self.cursor.execute(
+            "SELECT name FROM sqlite_master WHERE type='table' AND name='SONGS'"
+        )
+        return len(self.cursor.fetchall()) == 1
+
+    def __create_table(self) -> None:
+        self.cursor.execute(
+            "CREATE TABLE SONGS(ARTIST_ID INTEGER NOT NULL, ALBUM_ID INTEGER NOT NULL, SONG_ID INTEGER NOT NULL, PRIMARY KEY(ARTIST_ID, ALBUM_ID, SONG_ID));"
+        )
         self.connection.commit()
 
-    def remove_song(self, song_id: int) -> None:
-        self.cursor.execute(f"SELECT * FROM SONGS WHERE SONG_ID = {song_id}")
-        song = self.cursor.fetchall()
-        if len(song) != 0:
-            self.cursor.execute(f"DELETE FROM SONGS WHERE SONG_ID = {song_id}")
-            self.connection.commit()
+    def __delete_table(self) -> None:
+        self.cursor.execute("DROP TABLE SONGS")
+        self.connection.commit()
+    
+    def __song_exists(self, artist_id: int, album_id: int, song_id: int) -> bool:
+        self.cursor.execute(f"SELECT FROM SONGS WHERE ARTIST_ID = {artist_id} AND ALBUM_ID = {album_id} AND SONG_ID = {song_id}")     
+        return len(self.cursor.fetchall()) != 0
+     
+    def __insert_song(self, artist_id: int, album_id: int, song_id: int) -> None:
+        self.cursor.execute(f"INSERT INTO SONGS(ARTIST_ID, ALBUM_ID, SONG_ID) VALUES({artist_id}, {album_id}, {song_id})")
+        self.connection.commit()
+
+    def __remove_song(self, artist_id: int, album_id: int, song_id: int) -> None:
+        self.cursor.execute(f"DELETE FROM SONGS WHERE ARTIST_ID = {artist_id} AND ALBUM_ID = {album_id} AND SONG_ID = {song_id}")     
+        self.connection.commit()  
 
     def get_songs(self) -> list[int]:
         self.cursor.execute(f"SELECT * FROM SONGS")
@@ -23,3 +43,9 @@ class Database:
 
     def close(self) -> None:
         self.connection.close()
+
+
+if __name__ == "__main__":
+    db = Database()
+    db.toggle_song(1, 1, 1)
+    db.close()
